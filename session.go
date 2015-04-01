c43ec2e618af2b85477cfd4ff5ddb3c4e1221d5b
cuixin <steven.cuixin@gmail.com>
2015-04-01 19:09:10 +0800

    fixed session bug

--- session.go
+++ session.go
@@ -8,7 +8,6 @@ import (
 	"os"
 	"sync"
 	"time"
-
 	msgpack "gopkg.in/vmihailenco/msgpack.v2"
 )
 
@@ -23,7 +22,8 @@ func NewSessionId(size int) string {
 
 // 用户会话
 type Session struct {
-	Sid            string        // 用户SessionId
+    mu sync.Mutex `msgpack:"-"`
+    Sid            string        // 用户SessionId
 	Uid            string        // 用户Uid
 	RemoteAddr     string        // 远程连接地址
 	ConnectTime    time.Time     // 连接时间
@@ -31,7 +31,36 @@ type Session struct {
 	PacketCount    int64         // 发送请求包的总数量
 	Attachment     interface{}   `msgpack:"-"` // 绑定的数据
 	DownQueue      *SafeQueue    `msgpack:"-"` // 下行数据的队列
-	MsgQueue       chan *Message `msgpack:"-"` // 消息
+	msgQ           chan *Message `msgpack:"-"` // 消息队列
+    isClosed       bool
+}
+
+func (this *Session) closeMsgQ() {
+    this.mu.Lock()
+    if !this.isClosed {
+        this.isClosed = true
+    }
+    close(this.msgQ)
+    this.mu.Unlock()
+}
+
+// 发送消息
+func (this *Session) SendMessage(msg *Message) bool {
+    isSend := false
+    this.mu.Lock()
+    if !this.isClosed {
+        isSend = true
+        this.msgQ <- msg
+        if msg.IsDown != nil {
+            msg.IsDown <- struct{}{}
+        }
+    }
+    this.mu.Unlock()
+    return isSend
+}
+
+func (this *Session) GetMsgQ() <-chan *Message {
+    return this.msgQ
 }
 
 // 实现一个双向唯一Sid<->Uid
@@ -49,7 +78,7 @@ type Message struct {
 }
 
 type SessionManager struct {
-	sync.Mutex
+	mu sync.Mutex
 	sidMaps map[string]*Session // SessionId ----> Session
 	uidMaps map[string]*Session // Uid       ----> Session
 }
@@ -58,6 +87,7 @@ func (this *SessionManager) NewSession(uid, remoteAddr string) (*Session, bool) 
 	sid := NewSessionId(32)
 	nowTime := time.Now()
 	s := &Session{
+        mu : sync.Mutex{},
 		Sid:            sid,
 		Uid:            uid,
 		RemoteAddr:     remoteAddr,
@@ -65,49 +95,50 @@ func (this *SessionManager) NewSession(uid, remoteAddr string) (*Session, bool) 
 		LastPacketTime: nowTime,
 		PacketCount:    1,
 		DownQueue:      NewSafeQueue(),
-		MsgQueue:       make(chan *Message, 16),
+		msgQ:           make(chan *Message, 16),
+        isClosed:       false,
 	}
-	this.Lock()
+	this.mu.Lock()
 	if oldSession, ok := this.sidMaps[sid]; ok {
 		sid = NewSessionId(32)
 		if oldSession2, ok2 := this.sidMaps[sid]; ok2 {
-			this.Unlock()
+			this.mu.Unlock()
 			return oldSession2, false
 		} else {
 			s.Sid = sid
 			this.sidMaps[sid] = s
 			this.uidMaps[uid] = s
-			this.Unlock()
+			this.mu.Unlock()
 			return s, true
 		}
-		this.Unlock()
+		this.mu.Unlock()
 		return oldSession, false
 	}
 	if oldSession, ok := this.uidMaps[uid]; ok {
-		this.Unlock()
+		this.mu.Unlock()
 		return oldSession, false
 	}
 	this.sidMaps[sid] = s
 	this.uidMaps[uid] = s
-	this.Unlock()
+	this.mu.Unlock()
 	return s, true
 }
 
 func (this *SessionManager) GetAllSessionUids() []string {
-	this.Lock()
-	sLen := len(this.uidMaps)
+    this.mu.Lock()
+    sLen := len(this.uidMaps)
 	ret := make([]string, sLen)
 	i := 0
 	for uid, _ := range this.uidMaps {
 		ret[i] = uid
 		i++
 	}
-	this.Unlock()
+	this.mu.Unlock()
 	return ret
 }
 
 func (this *SessionManager) GetAllSessionSids() []string {
-	this.Lock()
+	this.mu.Lock()
 	sLen := len(this.sidMaps)
 	ret := make([]string, sLen)
 	i := 0
@@ -115,67 +146,67 @@ func (this *SessionManager) GetAllSessionSids() []string {
 		ret[i] = sid
 		i++
 	}
-	this.Unlock()
+	this.mu.Unlock()
 	return ret
 }
 
 func (this *SessionManager) ClearSession() {
-	this.Lock()
+	this.mu.Lock()
 	this.uidMaps = nil
 	this.uidMaps = make(map[string]*Session, 16<<10)
 	this.sidMaps = nil
 	this.sidMaps = make(map[string]*Session, 16<<10)
-	this.Unlock()
+	this.mu.Unlock()
 }
 
 func (this *SessionManager) GetSessionBySid(sid string) *Session {
-	this.Lock()
+	this.mu.Lock()
 	ret := this.sidMaps[sid]
-	this.Unlock()
+	this.mu.Unlock()
 	return ret
 }
 
 func (this *SessionManager) GetSessionByUid(uid string) *Session {
-	this.Lock()
+	this.mu.Lock()
 	ret := this.uidMaps[uid]
-	this.Unlock()
+	this.mu.Unlock()
 	return ret
 }
 
 func (this *SessionManager) GetSessionCount() int {
-	this.Lock()
+	this.mu.Lock()
 	ret := len(this.sidMaps)
-	this.Unlock()
+	this.mu.Unlock()
 	return ret
 }
 
 func (this *SessionManager) RemoveSession(s *Session) {
-	this.Lock()
+	this.mu.Lock()
 	delete(this.sidMaps, s.Sid)
 	delete(this.uidMaps, s.Uid)
-	close(s.MsgQueue)
-	this.Unlock()
+	this.mu.Unlock()
+    s.closeMsgQ()
 }
 
 func (this *SessionManager) RemoveSessionBySid(sid string) *Session {
-	this.Lock()
+	this.mu.Lock()
 	s, ok := this.sidMaps[sid]
 	if ok {
 		delete(this.sidMaps, sid)
 		delete(this.uidMaps, s.Uid)
 	}
-	this.Unlock()
+	this.mu.Unlock()
 	return s
 }
 
 func (this *SessionManager) RemoveSessionByUid(uid string) *Session {
-	this.Lock()
+	this.mu.Lock()
 	s, ok := this.uidMaps[uid]
 	if ok {
 		delete(this.sidMaps, s.Sid)
 		delete(this.uidMaps, uid)
 	}
-	this.Unlock()
+	this.mu.Unlock()
 	return s
 }
 
@@ -218,8 +249,10 @@ func (this *SessionManager) LoadFromFile(filePath string) (int, error) {
 	msgpack.Unmarshal(data, &this.sidMaps)
 	l1 := len(this.sidMaps)
 	for _, v := range this.sidMaps {
-		v.MsgQueue = make(chan *Message, 16)
+        v.mu = sync.Mutex{}
+		v.msgQ = make(chan *Message, 16)
 		v.DownQueue = NewSafeQueue()
+        v.isClosed = false
 		this.uidMaps[v.Uid] = v
 	}
 	return l1, nil
